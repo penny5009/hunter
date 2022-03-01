75d0d543624301c3e2f9fef45f5a6098dd093509
penny5009 <55205759+penny5009@users.noreply.github.com>
2022-03-01 11:21:39 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("" + (receivedNumber))
+})
+input.onButtonPressed(Button.A, function () {
+    target += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(target)
+    radio.sendValue("id", 15)
+    radio.sendValue("try", target)
+})
+input.onButtonPressed(Button.B, function () {
+    target += 1
+})
+let target = 0
 radio.setGroup(99)
-let id = 15
-let guess = 50
-basic.showString("try")
-basic.showNumber(guess)
+basic.showIcon(IconNames.Heart)
+target = 50
